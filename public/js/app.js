9b16509cb5b9ac173d2a8317992958e435cbaa08
José Carlos Santos <josecarlossantos@Joses-MacBook-Pro.local>
2018-08-29 15:26:15 -0300

    Looping to Objs

--- public/js/app.js
+++ public/js/app.js
@@ -44,7 +44,10 @@ const budgetController = (() =>{
         },
         testings:()=>{
 
-
+            let item = data.allItems;
+            for(var sex in item){
+                console.log(sex);
+            }
         }
     };
 })();
